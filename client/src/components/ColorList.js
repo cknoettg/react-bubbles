e1769ed082ddf59be2e46475c9e2e1afadcc0a33
cknoettg <cknoettg@yahoo.com>
2020-06-12 22:45:21 -0400

    Completed ColorList component

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import AxiosWithAuth from '../utils/AxiosWithAuth';
 
 const initialColor = {
   color: "",
@@ -21,10 +21,26 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+    .put(`/colors/${colorToEdit.id}`, colorToEdit) //.put(`http://localhost:5000/api/colors/${colorToEdit.id}`)
+    .then(res =>{
+      //console.log(res.data);
+      setColorToEdit(res.data)
+      setEditing(false);
+      // props.history.push(`/protected`) - delete this out
+    })
+    .catch(err => console.log(err))
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+    .delete(`/colors/${color.id}`) //.delete(`http://localhost:5000/api/colors/${color.id}`)
+    .then(res => {
+      setColorToEdit(res.data)
+      //props.history.push(`/protected`); - delete this out
+    })
+    .catch(err=> console.log(err))
   };
 
   return (
